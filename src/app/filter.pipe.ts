56dc6e64fe797d2b7478a352dcb5390a38aa160a
DT0998 <duongkytrieuvn@gmail.com>
2023-09-20 23:05:49 +0700

    feat: add smth

---
+++ src/app/filter.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'filter',
+})
+export class FilterPipe implements PipeTransform {
+  transform(value: any, filterString?: string, propsName?: string): any {
+    if (value.length === 0 || filterString === '') {
+      return value;
+    }
+    //  push the item to the resultArray if the item's propsName is equal to the filterString
+    const resultArray = [];
+    for (const item of value) {
+      if (item[propsName] === filterString) {
+        resultArray.push(item);
+      }
+    }
+    return resultArray;
+  }
+}
